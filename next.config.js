29ba542c46f915964e629a2fe472ac448c9c2c11
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-03-16 19:26:06 +0700

    new package.json

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: [
+    'www.thaiflames.app', 
+    'i0.wp.com', 
+    'thaiflames.app'
+    ],
+    unoptimized:false
+  }
 }
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  env: {
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL,
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
 
 module.exports = nextConfig
